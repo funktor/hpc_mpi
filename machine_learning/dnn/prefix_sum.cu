f5a2caecab3dfae080c885e08f3e0f5df9548527
abhijit <abhi2iitk@gmail.com>
2025-07-26 16:54:52 +0000

    updating

--- machine_learning/dnn/prefix_sum.cu
+++ machine_learning/dnn/prefix_sum.cu
@@ -35,6 +35,7 @@
 #include <cuda_runtime.h>
 
 #define BLOCK_WIDTH 1024
+#define COARSE_FACTOR 4
 #define MAX_N 1e7
 __device__ int counter=0;
 
@@ -70,8 +71,7 @@ void prefix_sum(float *arr, float *out, int n) {
 }
 
 __device__
-void prefix_sum_kogge_stone_block(float *arr, float *out, int n) {
-    __shared__ float XY[BLOCK_WIDTH];
+void prefix_sum_kogge_stone_block(float *arr, float *XY, int n) {
     unsigned int index = blockIdx.x*blockDim.x + threadIdx.x;
 
     if (index < n) XY[threadIdx.x] = arr[index]; 
@@ -86,16 +86,11 @@ void prefix_sum_kogge_stone_block(float *arr, float *out, int n) {
         XY[threadIdx.x] += temp;
         __syncthreads();
     }
-
-    if (index < n) out[index] = XY[threadIdx.x];
 }
 
 __device__
-void prefix_sum_kogge_stone_block_flagged(float *arr, float *out, int *flags, int n) {
-    __shared__ float XY[BLOCK_WIDTH];
+void prefix_sum_brent_kung_block(float *arr, float *XY, int n) {
     unsigned int index = blockIdx.x*blockDim.x + threadIdx.x;
-
-    while (flags[index] != 1) {}
 
     if (index < n) XY[threadIdx.x] = arr[index]; 
     else XY[threadIdx.x] = 0.0f;
@@ -103,56 +98,171 @@ void prefix_sum_kogge_stone_block_flagged(float *arr, float *out, int *flags, in
     __syncthreads();
 
     for (unsigned int stride = 1; stride < blockDim.x; stride *= 2) {
-        float temp = 0.0f;
-        if (threadIdx.x >= stride) temp = XY[threadIdx.x-stride];
+        int i = 2*(threadIdx.x+1)*stride-1;
+        if (i < BLOCK_WIDTH && i >= stride) XY[i] += XY[i-stride];
         __syncthreads();
-        XY[threadIdx.x] += temp;
+    }
+
+    for (unsigned int stride = BLOCK_WIDTH/4; stride > 0; stride /= 2) {
+        int i = 2*(threadIdx.x+1)*stride-1;
+        if (i + stride < BLOCK_WIDTH) XY[i + stride] += XY[i];
         __syncthreads();
+    }
+}
+
+__device__
+void prefix_sum_brent_kung_block_coarsened(float *arr, float *XY, int n) {
+    for (unsigned int i = threadIdx.x; i < COARSE_FACTOR*blockDim.x; i += blockDim.x) {
+        unsigned int index = COARSE_FACTOR*blockIdx.x*blockDim.x + i;
+        if (index < n) XY[i] = arr[index]; 
+        else XY[i] = 0.0f;
+    }
+
+    __syncthreads();
+
+    for (unsigned int stride = 1; stride < COARSE_FACTOR*blockDim.x; stride *= 2) {
+        for (unsigned int i = threadIdx.x; i < COARSE_FACTOR*blockDim.x; i += blockDim.x) {
+            int j = 2*(i+1)*stride-1;
+            if (j < COARSE_FACTOR*BLOCK_WIDTH && j >= stride) XY[j] += XY[j-stride];
+        }
+        __syncthreads();
+    }
+
+    for (unsigned int stride = COARSE_FACTOR*BLOCK_WIDTH/4; stride > 0; stride /= 2) {
+        for (unsigned int i = threadIdx.x; i < COARSE_FACTOR*blockDim.x; i += blockDim.x) {
+            int j = 2*(i+1)*stride-1;
+            if (j + stride < COARSE_FACTOR*BLOCK_WIDTH) XY[j + stride] += XY[j];
+        }
+        __syncthreads();
+    }
+}
+
+// 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15  0 - 0+1 -> 1
+//   1   3   5   7   9    11    13    15  1 - 2+3 -> 3 
+//       3       7        11          15  2 - 4+5 -> 5
+//               7                    15
+//                                    15
+//                        11              0 - 7 + 11 -> 11
+//           5       9          13        0 - 3 + 5 -> 5, 1 - 7 + 9 -> 9
+//     2   4   6   8   10    12    14
+
+__global__
+void prefix_sum(float *arr, float *out, int *flags, float *S, int n, int m) {
+    extern __shared__ float XY[];
+    unsigned int index = blockIdx.x*blockDim.x + threadIdx.x;
+
+    prefix_sum_brent_kung_block(arr, XY, n);
+
+    if (blockIdx.x + 1 < m && threadIdx.x == 0) {
+        while (atomicAdd(&flags[blockIdx.x], 0) == 0) {}
+        S[blockIdx.x + 1] = S[blockIdx.x] + XY[min(blockDim.x-1, n-1-blockIdx.x*blockDim.x)];
+        __threadfence();
+        atomicAdd(&flags[blockIdx.x + 1], 1);
+    }
+    __syncthreads();
+
+    if (blockIdx.x < m && index < n && blockIdx.x > 0) {
+        while (atomicAdd(&flags[blockIdx.x], 0) == 0) {}
+        XY[threadIdx.x] += S[blockIdx.x];
     }
 
     if (index < n) out[index] = XY[threadIdx.x];
 }
 
 __global__
-void prefix_sum_kogge_stone_p1(float *arr, float *out, int *flags, float *S, float *S_out, int n, int m) {
-    int index = blockIdx.x*blockDim.x + threadIdx.x;
-    prefix_sum_kogge_stone_block(arr, out, n);
-    __syncthreads();
-    if (blockIdx.x < m && threadIdx.x == 0) {
-        S[blockIdx.x] = out[min((blockIdx.x+1)*blockDim.x-1, n-1)];
+void prefix_sum_coarsened(float *arr, float *out, int *flags, float *S, int n, int m) {
+    extern __shared__ float XY[];
+
+    prefix_sum_brent_kung_block_coarsened(arr, XY, n);
+
+    if (blockIdx.x + 1 < m && threadIdx.x == 0) {
+        while (atomicAdd(&flags[blockIdx.x], 0) == 0) {}
+        S[blockIdx.x + 1] = S[blockIdx.x] + XY[min(COARSE_FACTOR*blockDim.x-1, n-1-COARSE_FACTOR*blockIdx.x*blockDim.x)];
         __threadfence();
-        flags[blockIdx.x] = 1;
+        atomicAdd(&flags[blockIdx.x + 1], 1);
     }
-    prefix_sum_kogge_stone_block_flagged(S, S_out, flags, m);
     __syncthreads();
-    if (index < n && blockIdx.x > 0) out[index] += S_out[blockIdx.x-1];
+
+    if (blockIdx.x < m && blockIdx.x > 0) {
+        while (atomicAdd(&flags[blockIdx.x], 0) == 0) {}
+
+        for (unsigned int i = threadIdx.x; i < COARSE_FACTOR*blockDim.x; i += blockDim.x) {
+            XY[i] += S[blockIdx.x];
+        }
+    }
+
+    for (unsigned int i = threadIdx.x; i < COARSE_FACTOR*blockDim.x; i += blockDim.x) {
+        unsigned int index = COARSE_FACTOR*blockIdx.x*blockDim.x + i;
+        if (index < n) out[index] = XY[i];
+    }
 }
 
 __global__
-void prefix_sum_kogge_stone_p2(float *S, float *S_out, int m) {
-    prefix_sum_kogge_stone_block(S, S_out, m);
-}
+void prefix_sum_coarsened_static(float *arr, float *out, int *flags, float *S, int n, int m) {
+    __shared__ float XY[COARSE_FACTOR*BLOCK_WIDTH];
 
-__global__
-void prefix_sum_kogge_stone_update(float *out, float *S_out, int n, int m) {
-    int index = blockIdx.x*blockDim.x + threadIdx.x;
-    if (index < n && blockIdx.x > 0) out[index] += S_out[blockIdx.x-1];
+    for (unsigned int i = threadIdx.x; i < COARSE_FACTOR*blockDim.x; i += blockDim.x) {
+        unsigned int index = COARSE_FACTOR*blockIdx.x*blockDim.x + i;
+        if (index < n) XY[i] = arr[index]; 
+        else XY[i] = 0.0f;
+    }
+
+    __syncthreads();
+
+    for (unsigned int stride = 1; stride < COARSE_FACTOR*blockDim.x; stride *= 2) {
+        for (unsigned int i = threadIdx.x; i < COARSE_FACTOR*blockDim.x; i += blockDim.x) {
+            int j = 2*(i+1)*stride-1;
+            if (j < COARSE_FACTOR*BLOCK_WIDTH && j >= stride) XY[j] += XY[j-stride];
+        }
+        __syncthreads();
+    }
+
+    for (unsigned int stride = COARSE_FACTOR*BLOCK_WIDTH/4; stride > 0; stride /= 2) {
+        for (unsigned int i = threadIdx.x; i < COARSE_FACTOR*blockDim.x; i += blockDim.x) {
+            int j = 2*(i+1)*stride-1;
+            if (j + stride < COARSE_FACTOR*BLOCK_WIDTH) XY[j + stride] += XY[j];
+        }
+        __syncthreads();
+    }
+
+    if (blockIdx.x + 1 < m && threadIdx.x == 0) {
+        while (atomicAdd(&flags[blockIdx.x], 0) == 0) {}
+        S[blockIdx.x + 1] = S[blockIdx.x] + XY[min(COARSE_FACTOR*blockDim.x-1, n-1-COARSE_FACTOR*blockIdx.x*blockDim.x)];
+        __threadfence();
+        atomicAdd(&flags[blockIdx.x + 1], 1);
+    }
+    __syncthreads();
+
+    if (blockIdx.x < m && blockIdx.x > 0) {
+        while (atomicAdd(&flags[blockIdx.x], 0) == 0) {}
+
+        for (unsigned int i = threadIdx.x; i < COARSE_FACTOR*blockDim.x; i += blockDim.x) {
+            XY[i] += S[blockIdx.x];
+        }
+    }
+
+    for (unsigned int i = threadIdx.x; i < COARSE_FACTOR*blockDim.x; i += blockDim.x) {
+        unsigned int index = COARSE_FACTOR*blockIdx.x*blockDim.x + i;
+        if (index < n) out[index] = XY[i];
+    }
 }
 
 int main(){
-    int n = 4096;
+    int n = 1e7;
     int m = int(ceil(float(n)/BLOCK_WIDTH));
 
-    float *a, *S, *S_out, *c1, *c2;
+    float *a, *S, *c1, *c2;
+    int *flags;
 
     cudaMallocManaged(&a, n*sizeof(float));
     cudaMallocManaged(&c1, n*sizeof(float));
     cudaMallocManaged(&c2, n*sizeof(float));
     cudaMallocManaged(&S, m*sizeof(float));
-    cudaMallocManaged(&S_out, m*sizeof(float));
+    cudaMallocManaged(&flags, m*sizeof(int));
 
     for (int i = 0; i < m; i++) S[i] = 0.0;
-    for (int i = 0; i < m; i++) S_out[i] = 0.0;
+    for (int i = 0; i < m; i++) flags[i] = 0;
+    flags[0] = 1;
 
     generate_data(a, n, 1);
 
@@ -167,12 +277,8 @@ int main(){
 
     start = std::chrono::high_resolution_clock::now();
 
-    prefix_sum_kogge_stone_p1<<<m, BLOCK_WIDTH>>>(a, c2, S, S_out, n, m);
+    prefix_sum_coarsened<<<m, BLOCK_WIDTH, BLOCK_WIDTH*COARSE_FACTOR*sizeof(float)>>>(a, c2, flags, S, n, m);
     cudaDeviceSynchronize();
-    // prefix_sum_kogge_stone_p2<<<m, BLOCK_WIDTH>>>(S, S_out, m);
-    // cudaDeviceSynchronize();
-    // prefix_sum_kogge_stone_update<<<m, BLOCK_WIDTH>>>(c2, S_out, n, m);
-    // cudaDeviceSynchronize();
 
     stop = std::chrono::high_resolution_clock::now();
     duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
